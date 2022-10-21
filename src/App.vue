5572b1673fa74ead39b7d55927991bc48aa72309
HsiaoHungWang <wang@ispan.com.tw>
2022-10-21 16:39:29 +0800

    套用 Bootstrap navbar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <script setup>
 import HelloVue from '@/components/HelloVue.vue'
-
+import Nav from './components/Nav.vue'
 </script>
 
-<template>
+<template> 
+  <Nav></Nav>
   <div class="row">
     <div class="col-3"><HelloVue></HelloVue> </div>
-    <div class="col-6">
-      <!--http://localhost:5174/-->
-     <router-link to="/">Home</router-link> |    <!--<a href="...">Home</a> -->
-      <!--http://localhost:5174/about-->
-      <router-link to="/about">About</router-link> | 
-      <!--http://localhost:5174/todo-->
-      <router-link to="/todo">Todo</router-link> | 
-      <hr />
+    <div class="col-6">   
+      <!--根據路由載入的component會顯示在這裡-->
       <router-view></router-view>
      
     </div>
